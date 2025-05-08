4744b51a039fb91d0a12a47fc6808de9be889689
busyhe <busyhe@qq.com>
2025-05-08 16:47:40 +0800

    fix: AuthError error type

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -124,12 +124,7 @@ export async function authenticate(
     await signIn('credentials', formData);
   } catch (error) {
     if (error instanceof AuthError) {
-      switch (error.type) {
-        case 'CredentialsSignin':
-          return 'Invalid credentials.';
-        default:
-          return 'Something went wrong.';
-      }
+      return error.message;
     }
     throw error;
   }
